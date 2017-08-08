26296e791c290e2ceb5f6d3460c1156f110660ae
Tasnuvaleeya <tasnuva2606@gmail.com>
2017-08-08 14:43:35 +0600

    used AJAX and CreateAPIView of rest_framework

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 # from .views import TweetDetailView, TweetListView,TweetCreateView,TweetUpdateView,TweetDeleteView
 from django.views.generic.base import RedirectView
-from .views import TweetListAPIView
+from .views import TweetListAPIView,TweetCreateAPIView
 
 urlpatterns = [
 
-    url(r'^$', TweetListAPIView.as_view(),name='list') #/api/tweet/
+    url(r'^$', TweetListAPIView.as_view(),name='list'),  #/api/tweet/
+    url(r'^create/',TweetCreateAPIView.as_view(),name='create'),
     # url(r'^$',RedirectView.as_view(url="/")),
     # url(r'^search/$',TweetListView.as_view(),name='list'),
     # url(r'^create/$',TweetCreateView.as_view(), name='create'),
