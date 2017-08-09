2a47a4196070a2c0fe22b2f0c3dde302e5699d01
Tasnuvaleeya <tasnuva2606@gmail.com>
2017-08-09 13:58:41 +0600

    added follower option and user profile

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')
+    # user.profile find the profile for the user
+    following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by')
+    # profile.user.following user i follow
+    # user.followed_by user that follow me
+
+    def __str__(self):
+        return str(self.following.all().count())
