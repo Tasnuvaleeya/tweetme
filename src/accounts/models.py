de3e481cc28babef093e2ad042a23af9d56f471d
Tasnuvaleeya <tasnuva2606@gmail.com>
2017-08-09 14:26:52 +0600

    Model manager has been added for following

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -2,12 +2,31 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
+
+class UserProfileManager(models.Manager):
+    def all(self):
+        qs = self.get_queryset().all()
+        print(self)
+        print(self.instance)
+        try:
+            if self.instance:
+                qs = qs.exclude(user = self.instance)
+        except:
+            pass
+        return qs
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')
     # user.profile find the profile for the user
     following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by')
     # profile.user.following user i follow
     # user.followed_by user that follow me
+    objects = UserProfileManager()
 
     def __str__(self):
         return str(self.following.all().count())
+
+    def get_following(self):
+        users = self.following.all()
+        return users.exclude(username=self.user.username)
