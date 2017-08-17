9e293c250c0b29f844e3491b18fde203795b43c2
Tasnuvaleeya <tasnuva2606@gmail.com>
2017-08-17 20:53:21 +0600

    jquery hasgtag parsing

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -6,6 +6,7 @@ from .pagination import StandardResultPagination
 
 from tweets.models import Tweet
 
+
 class TweetCreateAPIView(generics.CreateAPIView):
     serializer_class = TweetModelSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -13,13 +14,16 @@ class TweetCreateAPIView(generics.CreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
     pagination_class = StandardResultPagination
 
     def get_queryset(self,*args,**kwargs):
         im_following = self.request.user.profile.get_following()
-        qs = Tweet.objects.filter(user__in=im_following).order_by('-timestamp')
+        qs1 = Tweet.objects.filter(user__in=im_following)
+        qs2 = Tweet.objects.filter(user=self.request.user)
+        qs = (qs1 | qs2).distinct().order_by('-timestamp')
         print(self.request.GET)
         query= self.request.GET.get("q",None)
         if query is not None:
