00a49fa65af294e6b1d8778d40b6147933f6df2c
Tasnuvaleeya <tasnuva2606@gmail.com>
2017-08-18 13:53:59 +0600

    retweet part 3

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.core.exceptions import ValidationError
+from django.utils import timezone
 from django.urls import reverse
 from .validators import validate_content
 
@@ -11,6 +11,15 @@ class TweetManager(models.Manager):
             og_parent = parent_obj.parent
         else:
             og_parent = parent_obj
+        qs = self.get_queryset().filter(
+            user=user, parent=parent_obj
+        ).filter(
+            timestamp__year=timezone.now().year,
+            timestamp__month=timezone.now().month,
+            timestamp__day=timezone.now().day,
+        )
+        if qs.exists():
+            return None
         obj = self.model(
             parent=og_parent,
             user=user,
