4e0b3d42149e7c8346513e2d262991fbd7345563
Tasnuvaleeya <tasnuva2606@gmail.com>
2017-08-18 15:36:52 +0600

    retweet with jQuery

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 # from .views import TweetDetailView, TweetListView,TweetCreateView,TweetUpdateView,TweetDeleteView
 from django.views.generic.base import RedirectView
-from .views import TweetListAPIView,TweetCreateAPIView
+from .views import TweetListAPIView, TweetCreateAPIView, RetweetAPIView
 
 urlpatterns = [
 
-    url(r'^$', TweetListAPIView.as_view(),name='list'),  #/api/tweet/
-    url(r'^create/', TweetCreateAPIView.as_view(), name='create'),
+    url(r'^$', TweetListAPIView.as_view(),name='list'),                 # /api/tweet/
+    url(r'^create/', TweetCreateAPIView.as_view(), name='create'),       # /api/tweet/create/
+    url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'),      # /api/tweet/id/retweet/
 
 ]
